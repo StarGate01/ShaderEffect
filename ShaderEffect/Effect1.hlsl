3d58994ef063a508948034cdb73687457b78d66e
Christoph Honal <christoph.honal@web.de>
2017-03-05 03:11:35 +0100

    renamed fx to hlsl

---
+++ ShaderEffect/Effect1.hlsl
@@ -0,0 +1,8 @@
+﻿float4 colorFilter : register(C0);
+sampler2D implicitInputSampler : register(S0);
+
+float4 main(float2 uv : TEXCOORD) : COLOR
+{
+	float4 color = tex2D(implicitInputSampler, uv);
+	return color * colorFilter;
+}
